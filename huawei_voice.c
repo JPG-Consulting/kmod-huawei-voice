a802edeba0cf1a3fdce62525ea73812d8c7db1c8
JPG-Consulting <opensource@jpg-consulting.es>
2015-07-26 06:56:07 +0200

    Update huawei_voice.c

--- huawei_voice.c
+++ huawei_voice.c
@@ -291,7 +291,7 @@ static int huawei_voice_probe(struct usb_serial *serial,
 	if (dev_desc->idVendor == cpu_to_le16(HUAWEI_VENDOR_ID) &&
 	    dev_desc->idProduct == cpu_to_le16(HUAWEI_PRODUCT_E600) &&
 	    serial->interface->cur_altsetting->desc.bInterfaceNumber != 1)
-		return -ENODEV;	*/
+		return -ENODEV;
     
 	/* Store device id so we can use it during attach. */
 	usb_set_serial_data(serial, (void *)id);
